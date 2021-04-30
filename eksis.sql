534dc17f0ce79438454f9d6a869102bdc113867f
adiswadnyana <73645133+adiswadnyana@users.noreply.github.com>
2021-04-30 20:30:47 +0800

    database

--- eksis.sql
+++ eksis.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 28/04/2021 22:59:34
+ Date: 30/04/2021 20:29:33
 */
 
 SET NAMES utf8mb4;
@@ -56,7 +56,7 @@ CREATE TABLE `roles`  (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `roles_name_unique`(`name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of roles
@@ -64,6 +64,7 @@ CREATE TABLE `roles`  (
 INSERT INTO `roles` VALUES (1, 'admin', 'Administrator', '2021-01-02 14:42:41', '2021-01-02 14:42:41');
 INSERT INTO `roles` VALUES (3, 'supervisor', 'Petugas Lapangan', '2021-01-28 06:04:21', '2021-04-28 09:28:33');
 INSERT INTO `roles` VALUES (4, 'accounting', 'Staff Keuangan', '2021-02-09 03:48:20', '2021-02-09 03:48:20');
+INSERT INTO `roles` VALUES (5, 'karyawan', 'karyawan', '2021-04-29 05:15:33', '2021-04-29 05:15:33');
 
 -- ----------------------------
 -- Table structure for tb_absensi
@@ -86,31 +87,16 @@ CREATE TABLE `tb_absensi`  (
   INDEX `tb_absensi_staff_id_foreign`(`staff_id`) USING BTREE,
   CONSTRAINT `tb_absensi_attendance_id_foreign` FOREIGN KEY (`attendance_id`) REFERENCES `tb_attendance` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tb_absensi_staff_id_foreign` FOREIGN KEY (`staff_id`) REFERENCES `tb_staff` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 472 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 513 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_absensi
 -- ----------------------------
-INSERT INTO `tb_absensi` VALUES (452, 27, 1, 0, NULL, 'januari-2021', '2021-01-01', '00:00:00', '00:00:00', '2021-04-28 05:09:53', '2021-04-28 05:09:53');
-INSERT INTO `tb_absensi` VALUES (453, 28, 1, 0, NULL, 'januari-2021', '2021-01-01', '00:00:00', '00:00:00', '2021-04-28 05:09:53', '2021-04-28 05:09:53');
-INSERT INTO `tb_absensi` VALUES (454, 29, 1, 0, NULL, 'januari-2021', '2021-01-01', '00:00:00', '00:00:00', '2021-04-28 05:09:53', '2021-04-28 05:09:53');
-INSERT INTO `tb_absensi` VALUES (455, 30, 1, 0, NULL, 'januari-2021', '2021-01-01', '00:00:00', '00:00:00', '2021-04-28 05:09:53', '2021-04-28 05:09:53');
-INSERT INTO `tb_absensi` VALUES (456, 27, 1, 0, NULL, 'januari-2021', '2021-01-02', '00:00:00', '00:00:00', '2021-04-28 05:10:22', '2021-04-28 05:10:22');
-INSERT INTO `tb_absensi` VALUES (457, 28, 3, 0, NULL, 'januari-2021', '2021-01-02', '00:00:00', '00:00:00', '2021-04-28 05:10:22', '2021-04-28 05:10:22');
-INSERT INTO `tb_absensi` VALUES (458, 29, 1, 0, NULL, 'januari-2021', '2021-01-02', '00:00:00', '00:00:00', '2021-04-28 05:10:22', '2021-04-28 05:10:22');
-INSERT INTO `tb_absensi` VALUES (459, 30, 2, 0, NULL, 'januari-2021', '2021-01-02', '00:00:00', '00:00:00', '2021-04-28 05:10:22', '2021-04-28 05:10:22');
-INSERT INTO `tb_absensi` VALUES (460, 27, 1, 0, NULL, 'januari-2021', '2021-01-03', '00:00:00', '00:00:00', '2021-04-28 05:10:36', '2021-04-28 05:10:36');
-INSERT INTO `tb_absensi` VALUES (461, 28, 1, 0, NULL, 'januari-2021', '2021-01-03', '00:00:00', '00:00:00', '2021-04-28 05:10:36', '2021-04-28 05:10:36');
-INSERT INTO `tb_absensi` VALUES (462, 29, 1, 0, NULL, 'januari-2021', '2021-01-03', '00:00:00', '00:00:00', '2021-04-28 05:10:36', '2021-04-28 05:10:36');
-INSERT INTO `tb_absensi` VALUES (463, 30, 1, 0, NULL, 'januari-2021', '2021-01-03', '00:00:00', '00:00:00', '2021-04-28 05:10:36', '2021-04-28 05:10:36');
-INSERT INTO `tb_absensi` VALUES (464, 27, 4, 0, NULL, 'januari-2021', '2021-04-28', '00:00:00', '00:00:00', '2021-04-28 05:53:18', '2021-04-28 09:35:01');
-INSERT INTO `tb_absensi` VALUES (465, 28, 1, 0, NULL, 'januari-2021', '2021-04-28', '00:00:00', '00:00:00', '2021-04-28 05:53:18', '2021-04-28 09:35:01');
-INSERT INTO `tb_absensi` VALUES (466, 29, 1, 0, NULL, 'januari-2021', '2021-04-28', '00:00:00', '00:00:00', '2021-04-28 05:53:18', '2021-04-28 09:35:01');
-INSERT INTO `tb_absensi` VALUES (467, 30, 2, 0, NULL, 'januari-2021', '2021-04-28', '00:00:00', '00:00:00', '2021-04-28 05:53:18', '2021-04-28 09:35:01');
-INSERT INTO `tb_absensi` VALUES (468, 27, 1, 0, NULL, 'februari-2021', '2021-02-01', '00:00:00', '00:00:00', '2021-04-28 09:44:23', '2021-04-28 09:44:23');
-INSERT INTO `tb_absensi` VALUES (469, 28, 1, 0, NULL, 'februari-2021', '2021-02-01', '00:00:00', '00:00:00', '2021-04-28 09:44:23', '2021-04-28 09:44:23');
-INSERT INTO `tb_absensi` VALUES (470, 29, 1, 0, NULL, 'februari-2021', '2021-02-01', '00:00:00', '00:00:00', '2021-04-28 09:44:23', '2021-04-28 09:44:23');
-INSERT INTO `tb_absensi` VALUES (471, 30, 2, 0, NULL, 'februari-2021', '2021-02-01', '00:00:00', '00:00:00', '2021-04-28 09:44:23', '2021-04-28 09:44:23');
+INSERT INTO `tb_absensi` VALUES (507, 27, 3, 0, NULL, 'januari-2021', '2021-04-29', '00:00:00', '00:00:00', '2021-04-29 08:20:17', '2021-04-30 00:51:22');
+INSERT INTO `tb_absensi` VALUES (508, 30, 1, 0, NULL, 'januari-2021', '2021-04-29', '00:00:00', '00:00:00', '2021-04-29 08:20:17', '2021-04-30 00:51:22');
+INSERT INTO `tb_absensi` VALUES (509, 28, 1, 0, NULL, 'januari-2021', '2021-04-29', '00:00:00', '00:00:00', '2021-04-29 08:20:41', '2021-04-29 08:25:21');
+INSERT INTO `tb_absensi` VALUES (511, 27, 1, 0, NULL, 'april-2021', '2021-04-29', '00:00:00', '00:00:00', '2021-04-30 00:37:14', '2021-04-30 00:37:14');
+INSERT INTO `tb_absensi` VALUES (512, 30, 1, 0, NULL, 'april-2021', '2021-04-29', '00:00:00', '00:00:00', '2021-04-30 00:37:14', '2021-04-30 00:37:14');
 
 -- ----------------------------
 -- Table structure for tb_attendance
@@ -169,7 +155,7 @@ CREATE TABLE `tb_departement`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_departement
@@ -178,6 +164,10 @@ INSERT INTO `tb_departement` VALUES (8, 'Monkey Forest', '2021-04-27 07:51:58', 
 INSERT INTO `tb_departement` VALUES (9, 'UBUD', '2021-04-27 09:05:49', '2021-04-27 09:05:49');
 INSERT INTO `tb_departement` VALUES (10, 'canggu', '2021-04-27 09:06:06', '2021-04-27 09:06:06');
 INSERT INTO `tb_departement` VALUES (11, 'JIMBARAN', '2021-04-27 09:06:26', '2021-04-27 09:06:26');
+INSERT INTO `tb_departement` VALUES (12, 'NYUH KUNING', '2021-04-30 01:49:25', '2021-04-30 01:49:25');
+INSERT INTO `tb_departement` VALUES (13, 'Accounting', '2021-04-30 02:10:05', '2021-04-30 02:10:05');
+INSERT INTO `tb_departement` VALUES (14, 'HRD', '2021-04-30 02:10:19', '2021-04-30 02:10:19');
+INSERT INTO `tb_departement` VALUES (15, 'Direktur', '2021-04-30 02:10:39', '2021-04-30 02:10:39');
 
 -- ----------------------------
 -- Table structure for tb_overtime
@@ -212,16 +202,17 @@ CREATE TABLE `tb_position`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_position
 -- ----------------------------
 INSERT INTO `tb_position` VALUES (7, 'Accounting', 'Staff', 5000000, 0, '2021-04-27 07:54:00', '2021-04-28 03:19:07');
-INSERT INTO `tb_position` VALUES (8, 'Pengawas Lapangan', 'Staff', 3500000, 0, '2021-04-27 07:54:33', '2021-04-27 07:54:33');
-INSERT INTO `tb_position` VALUES (9, 'Pekerja Lapangan', 'Daily Worker', 100000, 0, '2021-04-27 07:54:59', '2021-04-27 07:54:59');
-INSERT INTO `tb_position` VALUES (10, 'Driver', 'Staff', 2800000, 0, '2021-04-27 07:55:25', '2021-04-27 07:55:25');
+INSERT INTO `tb_position` VALUES (8, 'Supervisor/Pengawas Lapangan', 'Staff', 3500000, 0, '2021-04-27 07:54:33', '2021-04-30 02:14:47');
+INSERT INTO `tb_position` VALUES (9, 'Outsourcing', 'Daily Worker', 100000, 0, '2021-04-27 07:54:59', '2021-04-30 02:15:06');
+INSERT INTO `tb_position` VALUES (10, 'DRIVER', 'Staff', 3200000, 0, '2021-04-27 07:55:25', '2021-04-30 02:15:35');
 INSERT INTO `tb_position` VALUES (11, 'Direktur', 'Staff', 6000000, 0, '2021-04-28 03:18:50', '2021-04-28 03:18:50');
+INSERT INTO `tb_position` VALUES (12, 'HRD', 'Staff', 5000000, 0, '2021-04-30 02:16:06', '2021-04-30 02:16:06');
 
 -- ----------------------------
 -- Table structure for tb_salary
@@ -245,13 +236,16 @@ CREATE TABLE `tb_salary`  (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_salary
 -- ----------------------------
-INSERT INTO `tb_salary` VALUES (18, 27, 5000000, 0, 0, 0, 5000000, 0, 0, 'Januari-2021', 'Staff', NULL, '2021-04-01', '2021-04-28 05:57:46', '2021-04-28 05:57:46', NULL);
-INSERT INTO `tb_salary` VALUES (19, 28, 300000, 0, 0, 0, 300000, 0, 0, 'Januari-2021', 'Daily Worker', NULL, '2021-04-28', '2021-04-28 05:58:07', '2021-04-28 05:58:07', NULL);
+INSERT INTO `tb_salary` VALUES (23, 27, 5000000, 10000, 100000, 0, 5110000, 0, 0, 'Januari-2021', 'Staff', NULL, '2021-04-01', '2021-04-30 01:27:40', '2021-04-30 04:53:28', '2021-04-30 04:53:28');
+INSERT INTO `tb_salary` VALUES (24, 27, 5000000, 0, 0, 0, 5000000, 0, 0, 'Februari-2021', 'Staff', 'Dibayar', '2021-04-30', '2021-04-30 02:08:00', '2021-04-30 04:53:51', '2021-04-30 04:53:51');
+INSERT INTO `tb_salary` VALUES (25, 27, 5000000, 0, 0, 0, 5000000, 0, 0, 'Januari-2021', 'Staff', 'Dibayar', '2021-04-30', '2021-04-30 04:55:02', '2021-04-30 04:55:06', '2021-04-30 04:55:06');
+INSERT INTO `tb_salary` VALUES (26, 27, 5000000, 500000, 40000, 0, 5540000, 0, 0, 'Januari-2021', 'Staff', NULL, '2021-04-30', '2021-04-30 04:55:25', '2021-04-30 04:55:25', NULL);
+INSERT INTO `tb_salary` VALUES (27, 28, 100000, 0, 0, 0, 100000, 0, 0, 'Januari-2021', 'Daily Worker', 'Dibayar', '2021-04-30', '2021-04-30 05:12:54', '2021-04-30 05:12:54', NULL);
 
 -- ----------------------------
 -- Table structure for tb_salary_daily
@@ -343,15 +337,14 @@ CREATE TABLE `tb_staff`  (
   CONSTRAINT `tb_staff_departement_id_foreign` FOREIGN KEY (`departement_id`) REFERENCES `tb_departement` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tb_staff_position_id_foreign` FOREIGN KEY (`position_id`) REFERENCES `tb_position` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tb_staff_users_id_foreign` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_staff
 -- ----------------------------
-INSERT INTO `tb_staff` VALUES (27, 7, 8, 25, 'I Kadek Adi Swadnyana', '1999-05-16', 'Jl. Pendidikan', '2021-02-11', '08968390806', NULL, '2021-04-27 07:56:45', '2021-04-27 07:56:45', NULL);
+INSERT INTO `tb_staff` VALUES (27, 7, 13, 25, 'I KADEK ADI SWADNYANA', '1999-05-16', 'Jl. Pendidikan', '2021-02-11', '08968390806', 'img/uploads/profile/99ca2f57c0a371162de3b95ba95d957b.png', '2021-04-27 07:56:45', '2021-04-30 03:38:49', NULL);
 INSERT INTO `tb_staff` VALUES (28, 9, 9, 27, 'DEWA AYU PUTRI NOVITA SARI', '1999-11-25', 'Test1', '2021-03-09', '081237760468', NULL, '2021-04-27 10:07:16', '2021-04-28 09:20:06', NULL);
-INSERT INTO `tb_staff` VALUES (29, 10, 10, NULL, 'I Ketut Anto', '2007-01-31', 'test1111', '2021-04-14', '081237760468', NULL, '2021-04-27 11:55:37', '2021-04-27 11:55:37', NULL);
-INSERT INTO `tb_staff` VALUES (30, 11, 8, 26, 'Putu Benny Adnyana', '1988-11-16', 'Jl Graha Wisata 3 no 93. Sidakarya', '2016-07-07', '087222939912', NULL, '2021-04-28 03:23:05', '2021-04-28 03:23:05', NULL);
+INSERT INTO `tb_staff` VALUES (30, 11, 15, 26, 'PUTU BENNY ADNYANA', '1988-11-16', 'Jl Graha Wisata 3 no 93. Sidakarya', '2016-07-07', '087222939912', NULL, '2021-04-28 03:23:05', '2021-04-30 03:38:29', NULL);
 
 -- ----------------------------
 -- Table structure for users
@@ -374,7 +367,7 @@ CREATE TABLE `users`  (
   UNIQUE INDEX `users_email_unique`(`email`) USING BTREE,
   INDEX `users_role_id_foreign`(`role_id`) USING BTREE,
   CONSTRAINT `users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
@@ -383,5 +376,6 @@ INSERT INTO `users` VALUES (24, 1, NULL, 'Adi Swadnyana', 'admin1', '$2y$10$Xgtw
 INSERT INTO `users` VALUES (25, 3, NULL, 'I Kadek Adi Swadnyana', 'adiswadnyana', '$2y$10$bYmi0QbvACQvv5mMyCN0luc8yrFdCStbOfRQp/rzEMbIseNolcAwm', NULL, NULL, '2021-04-27 07:56:45', '2021-04-27 07:56:45', NULL);
 INSERT INTO `users` VALUES (26, 1, NULL, 'Putu Benny Adnyana', 'bennyadnyana', '$2y$10$KclbkN2w63548pFcEH21aehIcGIB2HwM0P/ilHJ6ZCnztyZvcU2ZO', NULL, NULL, '2021-04-28 03:23:05', '2021-04-28 03:23:05', NULL);
 INSERT INTO `users` VALUES (27, 4, NULL, 'DEWA AYU PUTRI NOVITA SARI', 'dewaayu', '$2y$10$xdt1Dk/z05f3erc3e2Rq3e0PRvg1f/YNvncBdvsRHt6auDNG9Xyju', NULL, NULL, '2021-04-28 09:20:06', '2021-04-28 09:20:06', NULL);
+INSERT INTO `users` VALUES (28, 5, NULL, 'I Made Licig', 'karyawan', '$2y$10$qx/FlxVYnGvFWwgECFWmmuKreJ0lrUw0WqbItGEaTB2UmqiN/GnWq', NULL, NULL, '2021-04-29 05:16:39', '2021-04-29 05:16:39', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
